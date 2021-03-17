67601ed4b868fa373b4d08b810123fb50088fc7f
Wild-Donkey <fysh4202@163.com>
2021-03-17 19:12:54 +0800

    03-17-2021

---
+++ Luogu/P2000-P2499/P2120 Storehouse_Building.cpp
@@ -0,0 +1,63 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <vector>
+#define Wild_Donkey 0
+using namespace std;
+inline unsigned RD() {
+  unsigned intmp = 0;
+  char rdch(getchar());
+  while (rdch < '0' || rdch > '9') {
+    rdch = getchar();
+  }
+  while (rdch >= '0' && rdch <= '9') {
+    intmp = intmp * 10 + rdch - '0';
+    rdch = getchar();
+  }
+  return intmp;
+}
+struct Factory {
+  long long x, p, c, sump, sumpx, K, f;
+}F[1000005];
+struct Hull {
+  long long x, y;
+  unsigned Ad;
+}H[1000005], Then;
+unsigned a[10005], now, n, l(1), r(1);
+bool b[10005];
+int main() {
+  n = RD();
+  for (register unsigned i(1); i <= n; ++i) {
+    F[i].x = RD();
+    F[i].p = RD();
+    F[i].c = RD();
+    F[i].sump = F[i - 1].sump + F[i].p;
+    F[i].sumpx = F[i - 1].sumpx + F[i].p * F[i].x;
+    F[i].K = F[i].sumpx - F[i].sump * F[i].x - F[i].c;
+  }
+  for (register unsigned i(1); i <= n; ++i) {
+    while (l < r && ((H[l + 1].y - H[l].y) < (F[i].x) * (H[l + 1].x - H[l].x))) {
+      ++l;
+    }
+    now = H[l].Ad;
+    F[i].f = F[now].f + F[i].c + (F[i].sump - F[now].sump) * F[i].x - F[i].sumpx + F[now].sumpx;
+    Then.Ad = i;
+    Then.x = F[i].sump;
+    Then.y = F[i].f + F[i].sumpx;
+    while (l < r && ((Then.y - H[r].y) * (H[r].x - H[r - 1].x) < (H[r].y - H[r - 1].y) * (Then.x - H[r].x))) {
+      --r; 
+    }
+    H[++r] = Then;
+  }
+  printf("%lld\n", F[n].f);
+  return Wild_Donkey;
+}
+
+
+
--- Luogu/P5000-P6999/P2365 Mission_Arrangement(N^2).cpp
+++ Luogu/P5000-P6999/P2365 Mission_Arrangement(N^2).cpp
@@ -40,44 +40,36 @@ inline int RDsg() {
 }
 struct Ms {
   long long C, T, SumC, SumT, f;
-}M[300005];
+}M[5005]; // 任务属性 
 struct Hull {
   long long x, y;
   unsigned Ad;
-}H[300005], *Now, Then;
+}H[5005], *Now, Then; // 下凸壳 
 unsigned n, l(1), r(1);
 long long S, Cst; 
-Hull *Binary (unsigned L, unsigned R, const long long &key) {
-  if(L == R) {
-    return H + L;
-  }
-  unsigned M((L + R) >> 1), M_ = M + 1;
-  if((H[M_].y - H[M].y) < key * (H[M_].x - H[M].x)) {//Key too big 
-    return Binary(M_, R, key);
-  }
-  return Binary(L, M, key);
-}
 int main() {
   n = RD();
   S = RD();
   M[0].SumT = S;
   for (register unsigned i(1); i <= n; ++i) {
-    M[i].T = RDsg();
+    M[i].T = RD();
     M[i].C = RD();
     M[i].SumT = M[i - 1].SumT + M[i].T;
-    M[i].SumC = M[i - 1].SumC + M[i].C;
+    M[i].SumC = M[i - 1].SumC + M[i].C; //预处理 
   }
-  Cst = S * M[n].SumC;
+  Cst = S * M[n].SumC;  // 截距中的一项常数 
   for (register unsigned i(1); i <= n; ++i) {
-    Now = Binary(l, r, M[i].SumT);
-    M[i].f = M[Now->Ad].f + (M[i].SumC - M[Now->Ad].SumC) * M[i].SumT + Cst - M[i].SumC * S;
+    while (l < r && ((H[l + 1].y - H[l].y) < M[i].SumT * (H[l + 1].x - H[l].x))) {
+      ++l; // 弹出过气决策点 
+    }
+    M[i].f = M[H[l].Ad].f + (M[i].SumC - M[H[l].Ad].SumC) * M[i].SumT + Cst - M[i].SumC * S; // 转移 
     Then.Ad = i;
     Then.x = M[i].SumC;
-    Then.y = M[i].f;
+    Then.y = M[i].f;    // 求新点坐标 
     while (l < r && ((Then.y - H[r].y) * (H[r].x - H[r - 1].x) <= (H[r].y - H[r - 1].y) * (Then.x - H[r].x))) {
-      --r;
+      --r; // 维护下凸 
     }
-    H[++r] = Then;
+    H[++r] = Then;      // 入队 
   }
   printf("%lld\n", M[n].f);
   return Wild_Donkey;
