a0ab416a4601e9a5512d1d594fef0386430317e4
Wild_Donkey <Wild_Donkey57@Outlook.com>
2021-05-08 22:07:17 +0800

    05-08-2021

--- Test.cpp
+++ Test.cpp
@@ -5,14 +5,13 @@
 #include <cstring>
 #include <ctime>
 #include <iostream>
-#include <unordered_map>
 #include <map>
 #include <queue>
 #include <vector>
 #define Wild_Donkey 0
 using namespace std;
-inline unsigned long long RD() {
-  unsigned long long intmp = 0;
+inline unsigned RD() {
+  unsigned intmp = 0;
   char rdch(getchar());
   while (rdch < '0' || rdch > '9') {
     rdch = getchar();
@@ -39,35 +38,22 @@ inline int RDsg() {
   }
   return rdtp * rdsg;
 }
-long long m, n, Cnt(0), A, B, C, D, t, Ans(0), Tmp(0);
+unsigned a[10005], m, n, Cnt(0), A, B, C, D, t, Ans(0), Tmp(0);
 bool b[10005];
 inline void Clr() {}
-long long Exgcd(long long x, long long y, long long &X, long long &Y) {
-  if (y) {
-    int tmp(Exgcd(y, x % y, Y, X));
-    Y -= X * (x / y);
-    //    printf("Exgcd(%d, %d, %d, %d)\n", x, y, X, Y);
-    return tmp;
-  }
-  X = 1;
-  Y = 0;
-  //  printf("Exgcd(%d, %d, %d, %d)\n", x, y, X, Y);
-  return x;
-}
 int main() {
   // double Ti(clock()), Mti(0);
-  //   freopen("P5656_5.in", "r", stdin);
-  //   freopen("P5656.out", "w", stdout);
-  //  t = RD();
-  //  for (register unsigned T(1); T <= t; ++T){
-  //  Clr();
-  B = RD();
-  A = RD();
-  Exgcd(A, B, C, D);
-  //    printf("%ld %ld %lu\n", C, D, E);
-  C = ((((D - 1) % A) + A) % A) + 1;  // y_min
-  printf("%lu\n", C);
-  //  }
+  // freopen(".in", "r", stdin);
+  // freopen(".out", "w", stdout);
+//  t = RD();
+//  for (register unsigned T(1); T <= t; ++T){
+//  Clr();
+//  n = RD();
+  for (register unsigned i(1); i <= 1000; ++i) {
+    for (register unsigned j(1); j <= 1000000000; ++j) {}
+    printf("%llu\n", i * (unsigned long long)1000000000);
+  }
+//  }
   // Ti = clock() - Ti;
   // printf("Time %lf MTime %lf\n", Ti, Mti);
   // system("pause");
@@ -75,8 +61,4 @@ int main() {
   // fclose(stdout);
   return Wild_Donkey;
 }
-/*
-49555 59069 92955
-33312 21610
-
-*/
+// Main PC 233.7s
