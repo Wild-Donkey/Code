77fbfc9c9e9cf92da46a8c44b42c1ddb12d55993
Wild-Donkey <fysh4202@163.com>
2021-05-26 21:55:15 +0800

    26-05-2021

--- CF/CF793F_Julia_the_snail_AC.cpp
+++ CF/CF793F_Julia_the_snail_AC.cpp
@@ -1,127 +1,68 @@
-#include <algorithm>
 #include <cmath>
 #include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <ctime>
 #include <iostream>
-#include <map>
-#include <queue>
-#include <vector>
 #define Wild_Donkey 0
 using namespace std;
 inline unsigned RD() {
-  unsigned intmp = 0;
-  char rdch(getchar());
-  while (rdch < '0' || rdch > '9') {
-    rdch = getchar();
-  }
-  while (rdch >= '0' && rdch <= '9') {
-    intmp = intmp * 10 + rdch - '0';
-    rdch = getchar();
-  }
+  unsigned intmp = 0;char rdch(getchar());
+  while (rdch < '0' || rdch > '9')rdch = getchar();
+  while (rdch >= '0' && rdch <= '9') intmp = intmp * 10 + rdch - '0', rdch = getchar();
   return intmp;
 }
-inline int RDsg() {
-  int rdtp(0), rdsg(1);
-  char rdch(getchar());
-  while ((rdch < '0' || rdch > '9') && (rdch != '-')) {
-    rdch = getchar();
-  }
-  if (rdch == '-') {
-    rdsg = -1;
-    rdch = getchar();
-  }
-  while (rdch >= '0' && rdch <= '9') {
-    rdtp = rdtp * 10 + rdch - '0';
-    rdch = getchar();
-  }
-  return rdtp * rdsg;
-}
-unsigned a[10005], A, B, m, n, q, Cnt(0), Ans[100005], Tmp(0), NowRi(0), To, From[100005], f[100005][400], BlockLen, BlockNum, Block[100005];
-struct Node {
-  Node *Son[2];
-  unsigned Val, TagMax; 
-}N[100005];
-struct Rope {
-  unsigned L, R;
-  inline const char operator < (const Rope &x) const {
-    return this->R < x.R;
-  }
-}R[100005];
-struct To {
-  unsigned Dest;
-  To *Nxt;
-}T[100005], *CntTo, *Out[100005];
+unsigned A[100005], B[100005], m, n, q, Ans(0), From[100005], f[100005][400], g[100005][400], BlockLen, BlockNum, Block[100405], Ql, Qr, Stack[100005];
 int main() {
-  // double Ti(clock()), Mti(0);
-  // freopen(".in", "r", stdin);
-  // freopen(".out", "w", stdout);
-//  t = RD();
-//  for (register unsigned T(1); T <= t; ++T){
-//  Clr();
   n = RD(), m = RD();
-  BlockLen = sqrt(n);
-  BlockNum = (n + BlockLen - 1) / BlockLen;
+  for (register unsigned i(1); i <= m; ++i) { // 先读进来, 因为求块长需要用 q 
+    A[i] = RD(), B[i] = RD();
+  }
+  q = RD();
+  BlockLen = (n / (sqrt(q) + 1)) + 1;       // 理论最优块长 
+  BlockNum = (n + BlockLen - 1) / BlockLen; // 块数 
   for (register unsigned i(1); i <= BlockNum; ++i) {
     for (register unsigned j(1); j <= BlockLen; ++j) {
-      Block[(i - 1) * BlockLen + j] = i;
+      Block[(i - 1) * BlockLen + j] = i;    // O(n) 预处理 Block 数组 
     }
   }
   for (register unsigned i(1); i <= m; ++i) {
-    R[i].L = RD(), R[i].R = RD();
-  }
-  sort(R + 1, R + m + 1);
-  for (register unsigned i(1); i <= m; ++i) {
-    (++CntTo)->Nxt = Out[R[i].L];
-    Out[R[i].L] = CntTo;
-    CntTo->Dest = R[i].R; 
-    From[R[i].L] = R[i].R;
-  }
-  q = RD(); 
-  for (register unsigned i(1); i <= q; ++i) {
-    Q[i].L = RD(), Q[i].R = RD(), Q[i].Num = i;
+    g[A[i]][Block[B[i]]] = max(g[A[i]][Block[B[i]]], B[i]); // 更新 g[l][Block[r]]
+    From[B[i]] = A[i];                                      // 存储 From[r] = l 
   }
   for (register unsigned i(1); i <= n; ++i) {
-    Ans[0] = i;
-    for (register unsigned j((i + BlockLen - 1) / BlockLen), Ri, k(i + 1); j <= BlockNum; ++j) {
-      Ri = j * BlockLen;
-      Ri = (Ri > n) ? n : Ri;
-      while (Out[i]->Nxt->Dest <= Ri) {
-        Out[i] = Out[i]->Nxt;
-      }
-      g[i][j] = Out[i]->Dest;
+    g[i][Block[i]] = max(i, g[i][Block[i]]);
+    for (register unsigned j(Block[i]); j <= BlockNum; ++j) {
+      g[i][j] = max(g[i][j], g[i][j - 1]);                  // 扫一遍 g 数组, 通过单调性处理 g 
     }
   }
-  for (register unsigned j(BlockNum), Ri; j >= 1; --j) {
+  for (register unsigned j(1), Ri; j <= BlockNum; ++j) {
     Ri = j * BlockLen;
-    Ri = (Ri > n) ? n : Ri;
-    for (register unsigned i())
-  }
-  for (register unsigned i(1), j; i <= q; ++i) {
-    if(Block[Q[i].L] ^ Block[Q[i].R]) {
-      Ans[0] = f[Q[i].L][Block[Q[i].R] - 1];
-      j = ((((Q[i].R + 1) / BlockLen) - 1) * BlockLen) + 1;
-    } else {
-      Ans[0] = Q[i].L;
-      j = Ans[0] + 1;
+    Ri = (Ri > n) ? n : Ri;                         // 终点的最大值 (第 j 块的右界)
+    for (register unsigned i(Ri), Hd(0); i >= 1; --i) {
+      while(Hd && Stack[Hd] <= g[i][j]) --Hd;       // 弹出队尾 
+      if(Stack[Hd + 1] <= g[i][j]) {                // 这个 i 弹出了至少一次队尾 
+        f[i][j] = max(f[Stack[Hd + 1]][j], g[i][j]);
+      }
+      else {                                        // 一次也没弹出 
+        f[i][j] = g[i][j];
+      }
+      Stack[++Hd] = i;
     }
-    while (j <= Q[i].R) {
-      if(Q[i].L <= From[j] && From[j] <= Ans[0]) {
-        Ans[0] = j;
+  }
+  for (register unsigned i(1), j; i <= q; ++i) {  // 回答询问 
+    Ql = RD(), Qr = RD();
+    if(Block[Ql] ^ Block[Qr]) {                   // 左右端点在不同块内 
+      Ans = f[Ql][Block[Qr] - 1];
+      j = ((Block[Qr] - 1) * BlockLen) + 1;
+    } else {                                      // 在同一块内 
+      Ans = Ql;
+      j = Ans + 1;
+    }
+    while (j <= Qr) {                             // 暴力最后不足一块的部分 
+      if(Ql <= From[j] && From[j] <= Ans) {
+        Ans = j;
       }
       ++j;
     }
-    printf("%u\n", Ans[0]);
+    printf("%u\n", Ans);
   }
-//  }
-  // Ti = clock() - Ti;
-  // printf("Time %lf MTime %lf\n", Ti, Mti);
-  // system("pause");
-  // fclose(stdin);
-  // fclose(stdout);
   return Wild_Donkey;
 }
-
-
-
