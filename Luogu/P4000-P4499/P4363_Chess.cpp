d8a60179839df4a275ac7019e2434bedc8be4248
Wild_Donkey <fysh4202@163.com>
2022-01-03 23:23:19 +0800

    1-3-2022

--- Luogu/P4000-P4499/P4363_Chess.cpp
+++ Luogu/P4000-P4499/P4363_Chess.cpp
@@ -37,7 +37,7 @@ int a[15][15], b[15][15], f[200005];
 unsigned List[200005], Step[200005];
 unsigned m, n, nm, Bnm;
 unsigned A, B, C, D, t;
-unsigned Cnt(0), Ans(0);
+unsigned Cnt(0), Ans(0), Tmp(0);
 inline unsigned PpC(unsigned x) {
   unsigned PRt(0);
   for (;x;x -= Lbt(x)) ++PRt;
@@ -60,36 +60,41 @@ signed main() {
   //  for (unsigned T(1); T <= t; ++T){
   //  Clr();
   n = RD(), m = RD(), Bnm = (1 << ((nm = n + m) - 1)) - 1;
-  for (unsigned i(1); i <= n; ++i) for (unsigned j(1); j <= m; ++j) a[i][j] = RD();
-  for (unsigned i(1); i <= n; ++i) for (unsigned j(1); j <= m; ++j) b[i][j] = RD();
-  DFS(0, 0, 0);
-  for (unsigned i(1); i <= Cnt; ++i) {
-    unsigned Now(List[i]);
-    for (unsigned j(0), Thi(n); j < nm; ++j) if (Now & (1 << j)) Step[i] += Thi; else --Thi;
+  if ((n * m) & 1) {
+    for (unsigned i(n); i; --i) for (unsigned j(m); j; --j) a[i][j] = RD();
+    for (unsigned i(n); i; --i) for (unsigned j(m); j; --j) b[i][j] = RD();
   }
-  for (unsigned i(2); i <= Cnt; ++i) f[i] = (Step[i] & 1) ? 0xafafafaf : 0x3f3f3f3f;
-  for (unsigned i(1); i < Cnt; ++i) {
-    unsigned Now(List[i]), Need((~Now) & (Now >> 1));
+  else {
+    for (unsigned i(n); i; --i) for (unsigned j(m); j; --j) b[i][j] = RD();
+    for (unsigned i(n); i; --i) for (unsigned j(m); j; --j) a[i][j] = RD();
+  }
+  DFS(0, 0, 0);
+  for (unsigned i(1); i <= Cnt; ++i) for (unsigned j(0), Thi(n); j < nm; ++j)
+    if (List[i] & (1 << j)) Step[i] += Thi; else --Thi;
+  for (unsigned i(2); i <= Cnt; ++i) {
+    unsigned Now(List[i]), Need(((~Now) >> 1) & Now & Bnm);
     if (Step[i] & 1) {
-      for (unsigned j(Need); j; j -= Lbt(j)) {
-        unsigned Dest(Find[Now ^ (3 * Lbt(j))]), Tmp, TmpN, Calc(f[i] - b[n + 1 - PpC((Lbt(j) - 1) & (~Now))][1 + PpC((Lbt(j) - 1) & Now)]);
-        // printf("Need %u, %u -> %u\n", Need, Dest, i);
-        if (Tmp > Calc)
-          // printf("Location (%u, %u)\n", n - PpC((Lbt(j) - 1) & (~List[Dest])), 1 + PpC((Lbt(j) - 1) & List[Dest]));
-      }
-    }
-    else {
+      f[i] = 0xafafafaf;
       for (unsigned j(Need); j; j -= Lbt(j)) {
         unsigned Dest(Find[Now ^ (3 * Lbt(j))]);
         // printf("Need %u, %u -> %u\n", Need, Dest, i);
-        f[Dest] = max(f[Dest], f[i] + a[n + 1 - PpC((Lbt(j) - 1) & (~Now))][1 + PpC((Lbt(j) - 1) & Now)]);
+        f[i] = max(f[i], f[Dest] + a[n - PpC((Lbt(j) - 1) & (~Now))][1 + PpC((Lbt(j) - 1) & Now)]);
+        // printf("Location (%u, %u)\n", n - PpC((Lbt(j) - 1) & (~List[Dest])), 1 + PpC((Lbt(j) - 1) & List[Dest]));
+      }
+    }
+    else {
+      f[i] = 0x3f3f3f3f;
+      for (unsigned j(Need); j; j -= Lbt(j)) {
+        unsigned Dest(Find[Now ^ (3 * Lbt(j))]);
+        // printf("Need %u, %u -> %u\n", Need, Dest, i);
+        f[i] = min(f[i], f[Dest] - b[n - PpC((Lbt(j) - 1) & (~Now))][1 + PpC((Lbt(j) - 1) & Now)]);
       }
     }
   }
-  for (unsigned i(1); i <= Cnt; ++i) printf("%u %u %u %d\n", i, List[i], Step[i], f[i]);
-  printf("%d\n", f[Cnt]);
+  // for (unsigned i(1); i <= Cnt; ++i) printf("%u %u %u %d\n", i, List[i], Step[i], f[i]);
+  printf("%d\n", ((n * m) & 1) ? f[Cnt] : (-f[Cnt]));
   //  }
-  system("pause");
+  // system("pause");
   return Wild_Donkey;
 }
 /*
